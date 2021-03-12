bff9adf1fd55d529e0bfeca5688ed0550879291c
faris <faris38kanbur@gmail.com>
2021-03-12 20:06:35 +0100

    add path!

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const path = require('path')
+
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: 'build.js'
+        // filename: 'build.js' if we write like this it is going to create file in dist
+        // filename: '../build/build.js'if we write like this it is going to create new doc and file in doc
+        
+        path: path.resolve(__dirname, 'build'),
+        // filename: 'build.js'
+        filename: 'js/build.js'
+    
     }
+
     
 
 }
