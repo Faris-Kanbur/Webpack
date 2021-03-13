81185fbfdf23c7404e6670c63e020acc379ce110
faris <faris38kanbur@gmail.com>
2021-03-13 23:10:18 +0100

    add library

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const path = require('path')
 
+/**
+ * [hash]
+ * [chunkhash]
+ * [name]
+ * [ıd]
+ * [query]
+ * [contenthash]
+ */
+
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: {
+        myfile: "./src/index.js"
+    },
     output: {
         // filename: 'build.js' if we write like this it is going to create file in dist
         // filename: '../build/build.js'if we write like this it is going to create new doc and file in doc
         
         path: path.resolve(__dirname, 'build'),
         // filename: 'build.js'
-        filename: 'js/build.js'
+        filename: 'js/[name].[contenthash].build.js',
+        publicPath: '/assets/',
+        // libraryTarget : 'amd',  // var-amd-commonjs-
+        // library: 'myfirstlibrary',
     
     }
 
